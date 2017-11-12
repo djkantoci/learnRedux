74d5cd1b99855a9f1bb532040914bc0956eb3ef0
Durdica Kantoci Buric <durdica.kantoci.buric@ecx.io>
2017-11-12 22:53:46 +0100

    Add action for adding/removing hobbies and movies

--- app/app.jsx
+++ app/app.jsx
@@ -15,5 +15,5 @@ require('style-loader!css-loader!sass-loader!applicationStyles');
 //   document.getElementById('app')
 // );
 
-//require('./redux-example.jsx');
-require('./redux-todo-example.jsx');
+require('./redux-example.jsx');
+//require('./redux-todo-example.jsx');
--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,16 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name : 'Anonymous'}, action) => {
+var stateDefault = {
+    name : 'Anonymous',
+    hobbies: [],
+    movies: []
+};
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
     //state = state || {name : 'Anonymous'};
 
     switch (action.type) {
@@ -11,6 +20,39 @@ var reducer = (state = {name : 'Anonymous'}, action) => {
                 ...state,
                 name: action.name
             };
+        case 'ADD_HOBBY':
+            return {
+                ...state,
+                hobbies: [
+                    ...state.hobbies,
+                    {
+                        id: nextHobbyId++,
+                        hobby: action.hobby
+                    }
+                ]
+            };    
+        case 'REMOVE_HOBBY':
+            return {
+                ...state,
+                hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id) // keeps every item that hasnt the same id
+            };                
+        case 'ADD_MOVIE':
+            return {
+                ...state,
+                movies: [
+                    ...state.movies,
+                    {
+                        id: nextMovieId++,
+                        title: action.title,
+                        genre: action.genre
+                    }
+                ]
+            };     
+        case 'REMOVE_MOVIE':
+            return {
+                ...state,
+                movies: state.movies.filter((movie) => movie.id !== action.id)
+            };                            
         default:
             return state;
     }
@@ -27,6 +69,8 @@ var unsubscribe = store.subscribe(() => {
     
     console.log('Name is', state.name);
     document.getElementById('app').innerHTML = state.name;
+    
+    console.log('New state', store.getState());
 });
 // unsubscribe();
 
@@ -39,6 +83,38 @@ store.dispatch({
 });
 
 store.dispatch({
+    type: 'ADD_HOBBY',
+    hobby: 'Running'
+});
+
+store.dispatch({
+    type: 'ADD_HOBBY',
+    hobby: 'Walking'
+});
+
+store.dispatch({
+    type: 'REMOVE_HOBBY',
+    id: 2
+});
+
+store.dispatch({
     type: 'CHANGE_NAME',
     name: 'Blabla'
+});
+
+store.dispatch({
+    type: 'ADD_MOVIE',
+    title: 'Pulp fiction',
+    genre: 'Who knows'
+});
+
+store.dispatch({
+    type: 'ADD_MOVIE',
+    title: 'La View en Rose',
+    genre: 'Drama'
+});
+
+store.dispatch({
+    type: 'REMOVE_MOVIE',
+    id: 1
 });
